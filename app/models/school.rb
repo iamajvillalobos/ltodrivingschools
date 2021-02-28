91b178141925aa99604e55e525b922a2dbbaac19
AJ Villalobos <hi@ajvillalobos.com>
2021-02-28 16:40:47 +0800

    feat: add school scopes

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,5 @@
 class School < ApplicationRecord
+  scope :filter_by_province, -> (province) { where province: province }
+  scope :filter_by_name, -> (name) { where name: name }
+  scope :filter_by_category, -> (category) { where category: category }
 end
